e7c4a6a73323847d1cc10158673d326e057caf6e
ehab-ahma <ehab24495@gmail.com>
2021-09-28 13:39:53 +0300

    adding the exam

--- src/components/FavFruit.js
+++ src/components/FavFruit.js
@@ -1,14 +1,89 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import { withAuth0 } from "@auth0/auth0-react";
+withAuth0;
 class FavFruit extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      favArr: [],
+    };
+  }
+  handeldelet = (frtId) => {
+    axios
+      .delete(`${process.env.REACT_APP_SERVER}/fav/${frtId}`)
+      .then((deletedFrt) => {
+        if (deletedFrt.data.deletedCount === 1) {
+          const newArr = this.state.favArr.filter(
+            (fruit) => fruit._id !== frtId
+          );
+          this.setState({ favArr: newArr });
+        }
+      })
+      .catch(() => alert("fruit was not deleted"));
+  };
+  updateFruit = (frt) => {
+    const reqBody = {
+      fruits_name: frt.name,
+      fruits_image: frt.image,
+      fruits_price: frt.price,
+      email: this.props.auth0.user.email,
+    };
+    axios
+      .put(`${process.env.REACT_APP_SERVER}/fav/${frt._id}`, reqBody)
+      .then((updated) => {
+        const newArr = this.state.favArr.map((ele) => {
+          if (ele._id == updated._id) {
+            return ele;
+          }
+          return ele;
+        });
+        this.setState({ favArr: newArr });
+      })
+      .catch(() => alert("data was not updated"));
+  };
+  componentDidMount() {
+    axios
+      .get(`${process.env.REACT_APP_SERVER}/fav`)
+      .then((favData) => {
+        this.setState({ favArr: favData.data });
+      })
+      .catch(() => alert("faveriote data was not recieved"));
+  }
   render() {
-    return(
+    return (
       <>
         <h1>My Favorite Fruits</h1>
+        <>
+          {this.state.favArr.map((frt) => {
+            return (
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={frt.image} />
+                <Card.Body>
+                  <Card.Title>{frt.name}</Card.Title>
+                  <Card.Text>{frt.price}</Card.Text>
+                  <Button
+                    variant="primary"
+                    onClick={() => this.updateFruit(frt._id)}
+                  >
+                    Update
+                  </Button>
+                  <Button
+                    variant="primary"
+                    onClick={() => this.handeldelet(frt)}
+                  >
+                    Delete
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </>
       </>
-    )
+    );
   }
 }
 
-export default FavFruit;
+export default withAuth0(FavFruit);
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,73 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { withAuth0 } from "@auth0/auth0-react";
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      fruitsArr: [],
+      favArr: [],
+    };
+  }
+  handelCreatFav = (fruit) => {
+    const reqBody = {
+      fruits_name: fruit.name,
+      fruits_image: fruit.image,
+      fruits_price: fruit.price,
+      email: this.props.auth0.user.email,
+    };
+    axios.post(`${process.env.REACT_APP_SERVER}/fav`, reqBody);
+    //   .then((createdFrt) => {
+    //     this.state.favArr.push(createdFrt.data);
+    //     // this.setState({ fruitsArr: this.state.fruitsArr });
+    //   })
+    //   .catch(() => {
+    //     alert("data was not creatd");
+    //   });
+  };
+  componentDidMount() {
+    axios
+      .get(`${process.env.REACT_APP_SERVER}/glopalFav`)
+      .then((res) => {
+        this.setState({ fruitsArr: res.data });
+      })
+      .catch(() => alert("data was not recieved"));
+  }
   render() {
+    console.log(this.state.fruitsArr);
+
     return (
       <>
-        <h1>API Fruits</h1>
+        <FavFruit favArr={this.state.favArr} />
+        {this.state.fruitsArr.length > 0 && (
+          <>
+            {this.state.fruitsArr.map((frt) => {
+              return (
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src={frt.image} />
+                  <Card.Body>
+                    <Card.Title>{frt.name}</Card.Title>
+                    <Card.Text>{frt.price}</Card.Text>
+                    <Button
+                      variant="primary"
+                      onClick={() => this.handelCreatFav(frt)}
+                    >
+                      Add-To-Fav
+                    </Button>
+                  </Card.Body>
+                </Card>
+              );
+            })}
+          </>
+        )}
       </>
-    )
+    );
   }
 }
 
-export default Home;
+export default withAuth0(Home);
